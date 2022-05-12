0e6eddd53e89cee21a2584e3ece795bc7938813f
ris2131 <inseok9876@gmail.com>
2022-05-13 00:25:03 +0900

    3.4.3.2 요구사항2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,9 +5,13 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -41,20 +45,41 @@ public class RequestHandler extends Thread {
             String[] tokens =  line.split(" ");
             File file = new File("./webapp"+tokens[1]);
             log.debug("run - file path : {}", "./webapp"+tokens[1]);
-            
-            //requestHeader 모두 빼기(request body 받을수있도록)
-            while( !"".equals(line) ){
-                line = br.readLine();
-                log.debug("run - requestHeader : {}",line);
+
+            String url = tokens[1];
+            String requestPath = url;
+            String params =null;
+            Map<String,String> queryString = new HashMap<>();
+
+            if(url.contains("?")){
+                requestPath = url.substring(0,url.indexOf("?"));
+                params = url.substring(url.indexOf("?")+1);
+                log.debug("run - urlLeft : {}", requestPath);
+                log.debug("run - urlRight : {}", params);
             }
-            
-            //body 내용 만들기.
-            byte[] body = Files.readAllBytes(file.toPath());
-            
-            //body header 만들어서 dos 에 담기
-            response200Header(dos, body.length);
-            //body 내용 dos 에 담기
-            responseBody(dos, body);
+
+            switch(requestPath){
+                case "/user/create":
+                    queryString = HttpRequestUtils.parseQueryString(params);
+                    User user = new User(queryString.get("userId"),queryString.get("password"),queryString.get("name"),queryString.get("email"));
+                    log.debug("User : {}",user.toString());
+                    break;
+                default:
+                    //requestHeader 모두 빼기(request body 받을수있도록)
+                    while( !"".equals(line) ){
+                        line = br.readLine();
+                        log.debug("run - requestHeader : {}",line);
+                    }
+                    //body 내용 만들기.
+                    byte[] body = Files.readAllBytes(file.toPath());
+                    //body header 만들어서 dos 에 담기
+                    response200Header(dos, body.length);
+                    //body 내용 dos 에 담기
+                    responseBody(dos, body);
+                    break;
+            }
+
+
 
             //br.close();
             
