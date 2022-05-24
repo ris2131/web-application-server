295130bcffcd7b2552668d7059f888dfc5dd6a1a
ris2131 <inseok9876@gmail.com>
2022-05-24 14:20:42 +0900

    [CODE REFACTORING]5.1.2.2 + 5.2.2 응답 데이터를 처리하는 로직을 별도의 클래스로 분리한다.(HttpResponse)
    - HttpResponse 클래스 추가 : 응답 데이터 처리 로직 클래스화.  header 를 처리하는 메서드를 통해 확장자에 일반적으로 돌아가게끔 처리, .js 확장자 추가.
    
    - HttpResponseTest 테스트클래스 추가.(수동, 관련 txt : resourcesHttp_Cookie.txt, resourcesHttp_Forward.txt, resourcesHttp_Redirect.txt)
    
    - requestHandler 코드 돌아가게끔 수정.(requestHandler 내 HttpResponse 클래스 넣어줌.)
    
    - RequestHandler 클래스 내 HttpResponse관련 메소드 삭제

---
+++ src/main/java/webserver/HttpResponse.java
@@ -0,0 +1,102 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+//응답 데이터 처리 클래스
+public class HttpResponse {
+
+    private DataOutputStream dos = null;
+    private static final Logger log = LoggerFactory.getLogger(HttpResponse.class);
+    //headers :  metaData 저장 하는 Map
+    Map<String, String> headers = new HashMap<>();
+    public HttpResponse(OutputStream out) {
+        dos = new DataOutputStream(out);
+    }
+
+
+    //forward : response 바로 뽑아주는것
+    public void forward(String url){
+        try {
+            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+            if(url.endsWith(".css")){
+                addHeader("Content-Type", "text/css");
+            } else if (url.endsWith("js")) {
+                addHeader("Content-Type","application/javascript");
+            } else{
+                addHeader("Content-Type","text/html;charset=utf-8");
+            }
+            addHeader("Content-Length",body.length+"");
+            response200Header(body.length);
+            responseBody(body);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    //sendRedirect : 302 header 이용해서 새로 보내도록 하는것.
+    //구 RequestHandler/response302Header()
+    public void sendRedirect(String url){
+        try{
+            dos.writeBytes("HTTP/1.1 302 REDIRECT \r\n");//Found? Rediect?
+            processHeaders();
+            dos.writeBytes("Location: " + url + "\r\n");//location을 추가 해주면, url 을 변경 해주네. "localhost:8080/index.html" 이 다시 실행 되는거지?(바로 ./webapp/index.html 을 찾는건 아닐거잖아?)
+            dos.writeBytes("\r\n");
+        }catch(IOException e){//이건 왜 쓰는거?
+            log.error(e.getMessage());
+        }
+    }
+    private void response200Header(int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            processHeaders();
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    private void responseBody(byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    //forwardBody 필요한지 모르겠다.. 안쓰이는거 같은데..
+    //아아... 테이블 바로 forward 띄울때 이거 쓰이겠다!
+    //url 로 forward 하는게 아니라 직접 html body 를 찍어주는 방식!
+    //구 RequestHandler/responseResource()
+    public void forwardBody(String body){
+        byte[] contents = body.getBytes();
+        addHeader("Content-Type","text/html;charset=utf-8");
+        addHeader("Content-Length",contents.length+"");
+        response200Header(contents.length);
+        responseBody(contents);
+    }
+    public void addHeader(String key, String val){
+        //headers 에 key,val 을 넣어주기
+        if(!headers.containsKey(key)){
+            headers.put(key,val);
+        }
+    }
+   public void processHeaders(){
+       try {
+           Set<String> keys = headers.keySet();
+            for(String key:keys){
+                dos.writeBytes(key+": "+headers.get(key)+"\r\n");
+            }
+       } catch (IOException e) {
+           log.error(e.getMessage());
+       }
+   }
+
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -31,163 +31,94 @@ public class RequestHandler extends Thread {
                 connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            //HTTP Request
             HttpRequest httpRequest = new HttpRequest(in);
 
-            //input 만들기
-            BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));//utf-8 설정 여기서.
-            //requestPath 에 따라 행동 변화
+            //header 에 긴 cookie 있으면 처리
             Map<String,String> cookieMap =null;//쿠키 여러개가 있을수 있어서 Map 으로 구성, ';' 로 떼는 Api 이용.
             if(httpRequest.getHeader("Cookie" ) != null ){
                 cookieMap = HttpRequestUtils.parseCookies(httpRequest.getHeader("Cookie"));
             }
-            //requestPath 확장자 에 따라
-            if(httpRequest.getPath().endsWith(".css")){
-                DataOutputStream dos = new DataOutputStream(out);//dos 는 뿌려줄때 최종적으로 저기 담는다.
-                byte[] body = Files.readAllBytes(new File("./webapp"+httpRequest.getPath()).toPath());
-                response200CssHeader(dos,body.length);
-                responseBody(dos,body);
-            }
-            else{
-                DataOutputStream dos = new DataOutputStream(out);//dos 는 뿌려줄때 최종적으로 저기 담는다.
-                switch(httpRequest.getPath()){
-                    //POST 로 수정.
-                    case "/user/create":
-                        User user = new User(httpRequest.getParameter("userId"),httpRequest.getParameter("password"),httpRequest.getParameter("name"),httpRequest.getParameter("email"));
-
-                        //작성자가 만든 저장 하는 api
-                        DataBase.addUser(user);
-                        log.debug("User : {} , DataBaseSize : {}",user.toString(),DataBase.findAll().size());
-                        response302Header(dos,"/index.html");
-                        break;
-
-                    /*
-                     * 로그인 하기
-                     * 로그인 성공하면 /index.html 이동, 로그인 실패하면 /user/login_failed.html 로 이동
-                     * post 방식
-                     * */
-                    case "/user/login":
-                        //POST : read body
-
-                        User tempUser = DataBase.findUserById(httpRequest.getParameter("userId"));//이렇게 넘겨 받는게 솔직히 엄청 위험해 보이긴 함..
-                        String redirectUrl = "/user/login_failed.html";
-                        //login 성공시 login.html
-                        if( tempUser != null && httpRequest.getParameter("password").equals(tempUser.getPassword()) ){//있고비번 같으면
-                            redirectUrl="/index.html";
-                            response302HeaderWithLoginSuccessHeader(dos,redirectUrl);
-                        } else{
-                            redirectUrl = "/user/login_failed.html";
-                            responseResource(out,redirectUrl);//200
-                        }
-                        break;
-                    //요구사항 6 : 사용자 목록 출력
-                    case "/user/list":
-                        String logined="false";//쿠키 자체가 없을수도 있어서 그떄도 false 처리 해야해서 이렇게 했음.
-                        if(cookieMap.containsKey("logined")){
-                            logined=cookieMap.get("logined");
-                        }
-                        if("true".equals(logined)){
-                            redirectUrl = "/user/list";//스위치문 지역변수 헷갈리네
-                            Collection<User> userList = DataBase.findAll();
-
-                            StringBuilder sb = new StringBuilder();
-                            sb.append("<table border='1'>");
-                            sb.append("<th>id</th>");
-                            sb.append("<th>pw</th>");
-                            sb.append("<th>name</th>");
-                            sb.append("<th>email</th>");
-                            //
-                            for(User u : userList) {
-                                sb.append("<tr>");
-                                sb.append("<td>"+u.getUserId()+"</td>");
-                                sb.append("<td>"+u.getPassword()+"</td>");
-                                sb.append("<td>"+u.getName()+"</td>");
-                                sb.append("<td>"+u.getEmail()+"</td>");
-                                sb.append("</tr>");
-                            }
-                            //
-                            sb.append("</table>");
-
-                            byte[] body = sb.toString().getBytes(StandardCharsets.UTF_8);
-                            response200HtmlHeader(dos,body.length);
-                            responseBody(dos,body);
-                        }else{
-                            redirectUrl = "/index.html";
-                            response302Header(dos,redirectUrl);
-                        }
-                        break;
-                    default:
-
-                        responseResource(out, httpRequest.getPath());
-                        break;
-                }
-            }
-
             
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+            //response 영역
+            HttpResponse httpResponse = new HttpResponse(out);
 
+            //requestPath 확장자 에 따라 => controller 역할
+            switch(httpRequest.getPath()){
+                //POST 로 수정.
+                case "/user/create":
+                    User user = new User(httpRequest.getParameter("userId"),httpRequest.getParameter("password"),httpRequest.getParameter("name"),httpRequest.getParameter("email"));
 
-    private void response200HtmlHeader(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
-    private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent ) {//map <string,>
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/css\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+                    //작성자가 만든 저장 하는 api
+                    DataBase.addUser(user);
+                    log.debug("User : {} , DataBaseSize : {}",user.toString(),DataBase.findAll().size());
 
-    //HTTP 응답상태 코드 302
-    //"Temporally moved" 상태!
-    //요청된 리소스가 임시적으로 이동페이지로 이동했다는 뜻.
-    //http status 마다 원하는 정보가 다르네!
-    private void response302Header(DataOutputStream dos , String url ){//length 는 필요 없나봐
-        try{
-            dos.writeBytes("HTTP/1.1 302 REDIRECT \r\n");
-            dos.writeBytes("Location: " + url + "\r\n");//location을 추가 해주면, url 을 변경 해주네. "localhost:8080/index.html" 이 다시 실행 되는거지?(바로 ./webapp/index.html 을 찾는건 아닐거잖아?)
-            dos.writeBytes("\r\n");
-            log.debug("HTTP REQUEST [302] : success");
-        }catch(IOException e){//이건 왜 쓰는거?
-            log.error(e.getMessage());
-        }
-    }
-    private void response302HeaderWithLoginSuccessHeader(DataOutputStream dos, String url) {
-        try{
-            dos.writeBytes("HTTP/1.1 302 REDIRECT \r\n");
-            dos.writeBytes("Set-cookie: logined=true \r\n");
-            dos.writeBytes("Location: " + url + "\r\n");//location을 추가 해주면, url 을 변경 해주네. "localhost:8080/index.html" 이 다시 실행 되는거지?(바로 ./webapp/index.html 을 찾는건 아닐거잖아?)
-            dos.writeBytes("\r\n");
-            log.debug("HTTP REQUEST [302] : success");
-        }catch(IOException e){//이건 왜 쓰는거?
-            log.error(e.getMessage());
-        }
-    }
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+                    //response302Header(dos,"/index.html");
+                    httpResponse.sendRedirect("/index.html");
+                    break;
+                case "/user/login":
+                    //POST : read body
+
+                    User tempUser = DataBase.findUserById(httpRequest.getParameter("userId"));//이렇게 넘겨 받는게 솔직히 엄청 위험해 보이긴 함..
+                    String redirectUrl = "/user/login_failed.html";
+                    //login 성공시 login.html
+                    if( tempUser != null && httpRequest.getParameter("password").equals(tempUser.getPassword()) ){//있고비번 같으면
+                        redirectUrl="/index.html";
+                        //response302HeaderWithLoginSuccessHeader(dos,redirectUrl);
+                        httpResponse.addHeader("Set-cookie", "logined=true");
+                        httpResponse.sendRedirect(redirectUrl);
+                    } else{
+                        redirectUrl = "/user/login_failed.html";
+                        //responseResource(out,redirectUrl);//200
+                        httpResponse.sendRedirect(redirectUrl);
+                    }
+                    break;
+                //요구사항 6 : 사용자 목록 출력
+                case "/user/list":
+                    String logined="false";//쿠키 자체가 없을수도 있어서 그떄도 false 처리 해야해서 이렇게 했음.
+                    if(cookieMap.containsKey("logined")){
+                        logined=cookieMap.get("logined");
+                    }
+                    if("true".equals(logined)){
+                        redirectUrl = "/user/list";//스위치문 지역변수 헷갈리네
+                        Collection<User> userList = DataBase.findAll();
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("<table border='1'>");
+                        sb.append("<th>id</th>");
+                        sb.append("<th>pw</th>");
+                        sb.append("<th>name</th>");
+                        sb.append("<th>email</th>");
+                        //
+                        for(User u : userList) {
+                            sb.append("<tr>");
+                            sb.append("<td>"+u.getUserId()+"</td>");
+                            sb.append("<td>"+u.getPassword()+"</td>");
+                            sb.append("<td>"+u.getName()+"</td>");
+                            sb.append("<td>"+u.getEmail()+"</td>");
+                            sb.append("</tr>");
+                        }
+                        //
+                        sb.append("</table>");
+
+                        //byte[] body = sb.toString().getBytes(StandardCharsets.UTF_8);
+                        //response200HtmlHeader(dos,body.length);
+                        //responseBody(dos,body);
+                        httpResponse.forwardBody(sb.toString());
+                    }else{
+                        redirectUrl = "/index.html";
+                        //response302Header(dos,redirectUrl);
+                        httpResponse.sendRedirect(redirectUrl);
+                    }
+                    break;
+                default:
+                    //responseResource(out, httpRequest.getPath());
+                    httpResponse.forward(httpRequest.getPath());
+                    break;
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-    //url resource 를 띄우는
-    private void responseResource(OutputStream out, String url) throws IOException {
-        DataOutputStream dos = new DataOutputStream(out);
-        byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
-        response200HtmlHeader(dos,body.length);
-        responseBody(dos,body);
     }
 }
